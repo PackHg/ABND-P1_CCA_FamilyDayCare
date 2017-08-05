c20f4b741107dc45f6d9a184de0b31d2e4fb20f6
Pack Heng <pack@oz-heng.com>
2017-08-06 00:03:53 +1000

    Click on address and url launches respectively Google Maps and CCA FDC web site  On branch master  Changes to be committed: 	modified:   app/src/main/java/com/oz_heng/apps/android/ccafamilydaycare/MainActivity.java 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/values/styles.xml

--- app/src/main/java/com/oz_heng/apps/android/ccafamilydaycare/MainActivity.java
+++ app/src/main/java/com/oz_heng/apps/android/ccafamilydaycare/MainActivity.java
@@ -1,14 +1,47 @@
 package com.oz_heng.apps.android.ccafamilydaycare;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Url of CCA FDC
+    private final String CCA_FDC_URL ="http://www.ccasfdc.org.au/";
+
+    // Url of CCA FDC on Google Maps
+    private final String CCA_FDC_ON_GOOGLE_MAPS = "https://www.google.com.au/maps/place/2+Dickson+Ave,+West+Ryde+NSW+2114/@-33.8062937,151.086607,17z/data=!4m13!1m7!3m6!1s0x6b12a44e1f2ed9b3:0xad57bc445457c27!2s2+Dickson+Ave,+West+Ryde+NSW+2114!3b1!8m2!3d-33.8065383!4d151.0865966!3m4!1s0x6b12a44e1f2ed9b3:0xad57bc445457c27!8m2!3d-33.8065383!4d151.0865966";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView addressTextView = (TextView) findViewById(R.id.org_address);
+        addressTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWebPage(CCA_FDC_ON_GOOGLE_MAPS);
+            }
+        });
+
+        TextView webUrlTextView = (TextView) findViewById(R.id.org_web_url);
+        webUrlTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWebPage(CCA_FDC_URL);
+            }
+        });
+    }
+
+    private void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
