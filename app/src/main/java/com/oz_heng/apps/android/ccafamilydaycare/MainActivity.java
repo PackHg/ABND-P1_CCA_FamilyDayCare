5f9d1e17beb3d09ec6f1c180ecf25d4469a59e16
Pack Heng <pack@oz-heng.com>
2017-08-06 01:05:41 +1000

    Click on phone number lauches a phone app for dialling the number  On branch master  Changes to be committed: 	modified:   app/src/main/java/com/oz_heng/apps/android/ccafamilydaycare/MainActivity.java

--- app/src/main/java/com/oz_heng/apps/android/ccafamilydaycare/MainActivity.java
+++ app/src/main/java/com/oz_heng/apps/android/ccafamilydaycare/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private final String CCA_FDC_EMAIL_ADDRESS = "fdc@ccas.org.au";
     // Url of CCA FDC on Google Maps
     private final String CCA_FDC_ON_GOOGLE_MAPS = "https://www.google.com.au/maps/place/2+Dickson+Ave,+West+Ryde+NSW+2114/@-33.8062937,151.086607,17z/data=!4m13!1m7!3m6!1s0x6b12a44e1f2ed9b3:0xad57bc445457c27!2s2+Dickson+Ave,+West+Ryde+NSW+2114!3b1!8m2!3d-33.8065383!4d151.0865966!3m4!1s0x6b12a44e1f2ed9b3:0xad57bc445457c27!8m2!3d-33.8065383!4d151.0865966";
+    // CCA FDC phone number
+    private final String CCA_FDC_PHONE_NUMBER = "+61298740066";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
                 composeEmail(CCA_FDC_EMAIL_ADDRESS, "Enquiry");
             }
         });
+
+        TextView phone = (TextView) findViewById(R.id.org_call);
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialPhoneNumber(CCA_FDC_PHONE_NUMBER);
+            }
+        });
     }
 
     private void openWebPage(String url) {
@@ -59,6 +69,14 @@ public class MainActivity extends AppCompatActivity {
         intent.setType("*/*");
         intent.setData(Uri.parse("mailto:" + address)); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
